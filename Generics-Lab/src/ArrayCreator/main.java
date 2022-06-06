12cebc05295fe2f079b6e61602b39a93a617adf0
Yordan Milanov <106768384+YordanMilanov@users.noreply.github.com>
2022-06-06 11:41:30 +0300

    Delete Generics-Lab/src/ArrayCreator directory

--- Generics-Lab/src/ArrayCreator/main.java
+++
@@ -1,9 +0,0 @@
-package ArrayCreator;
-
-import java.util.Scanner;
-
-public class main {
-    public static void main(String[] args) {
-        ArrayCreator arrayCreator = new ArrayCreator();
-    }
-}
