a97781c7c8900e48704d3a86561f6c7cae7cf6eb
YordanMilanov <dancho.galinov@gmail.com>
2022-06-20 15:52:27 +0300

    added 5th task

---
+++ 18.IteratorsAndComparators-Exercise/src/ComparingObjects_05/Main.java
@@ -0,0 +1,43 @@
+package ComparingObjects_05;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        String input = scanner.nextLine();
+        List<Person> people = new ArrayList<>();
+
+        while(!input.equals("END")) {
+            String [] data = input.split(" ");
+            String name = data[0];
+            int age = Integer.parseInt(data[1]);
+            String town = data[2];
+
+            Person person = new Person(name, age, town);
+            people.add(person);
+            input = scanner.nextLine();
+        }
+        int comparePersonIndex = Integer.parseInt(scanner.nextLine());
+        Person personToCompare = people.get(comparePersonIndex - 1);
+        int totalPeople = people.size();
+        int samePeople = 0;
+        int differentPeople = 0;
+
+        for (Person person : people) {
+            if (person.compareTo(personToCompare) == 0) {
+                samePeople++;
+            } else {
+                differentPeople++;
+            }
+        }
+        if (samePeople == 1) {
+            System.out.println("No matches");
+        } else {
+            System.out.printf("%d %d %d",samePeople, differentPeople, totalPeople);
+        }
+    }
+}
